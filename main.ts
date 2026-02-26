20a0f03f5f11c92a29e4a72ee11d851094be14d8
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-26 13:29:00 -0500

    added comments

--- main.ts
+++ main.ts
@@ -5,15 +5,18 @@
  * This program lights up an led when you click the "a" button and turns it off when you click the "b" button
  */
 
+// setup
 basic.clearScreen()
 pins.digitalWritePin(DigitalPin.P16, 0)
 basic.showIcon(IconNames.Happy)
 
+// turn on LED
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.showIcon(IconNames.Yes)
 })
 
+// turn off LED
 input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P16, 0)
     basic.showIcon(IconNames.No)
